c6e1bc37189ff99e64f3c59e23ef2aff7d813d8b
MERCURY <wanasart2@gmail.com>
2025-08-14 15:32:22 +0700

    feat(error): จัดการ error 404,500

---
+++ client/my-app/src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ServerError() {
+  return (
+    <div>
+      <h1>500 - Server Error</h1>
+      <p>Something went wrong on our end. Please try again later.</p>
+    </div>
+  );
+}
