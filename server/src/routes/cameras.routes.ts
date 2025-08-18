3fcf0e17532cd7410e43003bb1e994b493c5beb3
zomdoi <naphat2706@gmail.com>
2025-08-18 18:45:06 +0700

    refactor(cameras): change router.post to put

--- server/src/routes/cameras.routes.ts
+++ server/src/routes/cameras.routes.ts
@@ -6,9 +6,9 @@
  *  - GET /api/cameras/total                            → ดึงจำนวนกล้องทั้งหมด
  *  - GET /api/cameras/:cam_id/maintenance              → ดึงข้อมูลการบำรุงรักษาของกล้องตาม cam_id
  *  - GET /api/cameras/maintenance                      → ดึงรายการประวัติการซ่อมบำรุงกล้องทั้งหมด
- *  - POST /api/maintenance_history/:cam_id/create      → สร้าง Maintenance History ใหม่
- *  - POST /api/maintenance_history/:cam_id/delete      → ลบ Maintenance History
- *  - POST /api/maintenance_history/:cam_id/update      → อัพเดท Maintenance History
+ *  - POST /api/ameras/:cam_id/maintenance/create       → สร้าง Maintenance History ใหม่
+ *  - PATCH /api/ameras/:cam_id/maintenance/delete      → ลบ Maintenance History
+ *  - PUT /api/ameras/:cam_id/maintenance/update        → อัพเดท Maintenance History
  *  - GET /api/cameras/event-detection                  → ดึงรายการ EventDetection ทั้งหมด
  *  - POST /api/events/createDetect                     → สร้าง EventDetect 
  *  - PUT /api/cameras/event-detection/:cds_id/update   → แก้ไข EventDetection ที่เลือก
@@ -35,8 +35,8 @@ router.get('/total', ctrl.total);
 router.get('/:cam_id/maintenance',ctrl.listMaintenanceByCamId);
 router.get('/maintenance',ctrl.listMaintenance);
 router.post('/:cam_id/maintenance/create', ctrl.createMaintenance);
-router.post('/:cam_id/maintenance/delete', ctrl.softDeleteMaintenance);
-router.post('/:cam_id/maintenance/update', ctrl.updateMaintenance);
+router.patch('/:cam_id/maintenance/delete', ctrl.softDeleteMaintenance);
+router.put('/:cam_id/maintenance/update', ctrl.updateMaintenance);
 
 router.get('/event-detection', ctrl.listEventDetection);
 router.post('/createDetect',ctrl.createEventDetection);
