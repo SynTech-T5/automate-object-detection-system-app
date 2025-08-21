67aa34a7574aecca1c67beaac72ab24572b6e387
MERCURY <wanasart2@gmail.com>
2025-08-21 22:33:31 +0700

    refactor: ย้ายที่ไฟล์

--- client/my-app/src/app/(with-layout)/alerts/page.tsx
+++ client/my-app/src/app/(with-layout)/alerts/page.tsx
@@ -1,59 +1,11 @@
-'use client';
+import AlertTable from "../../components/AlertTable";
 
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
-import LogoutButton from "../../components/LogoutButton";
-
-export default function AlertsPage() {
-    const onConfirm = () => {
-        window.location.href = "/cameras"; // เปลี่ยนเส้นทางไปยังหน้า cameras
-        // TODO: เรียก API / ทำงานที่ต้องการ
-    };
-
+export default async function AlertsPage() {
     return (
-        <div>
-            <h1>Alerts Page</h1>
-
-            <AlertDialog>
-                <AlertDialogTrigger asChild>
-                    <Button className="bg-[#0077FF] text-white hover:bg-[#0063d6]">
-                        Show Dialog
-                    </Button>
-                </AlertDialogTrigger>
-
-                <AlertDialogContent>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>You want to go to the Cameras menu, sure?</AlertDialogTitle>
-                        <AlertDialogDescription>…</AlertDialogDescription>
-                    </AlertDialogHeader>
-
-                    <AlertDialogFooter>
-                        <AlertDialogCancel className="border-gray-300 hover:bg-gray-50">
-                            Cancel
-                        </AlertDialogCancel>
-
-                        {/* ไม่ต้อง asChild ก็ได้ ใส่ onClick ตรงนี้เลย จะปิด dialog ให้เอง */}
-                        <AlertDialogAction
-                            className="bg-[var(--color-primary)] text-white hover:bg-[var(--color-secondary)] "
-                            onClick={onConfirm}
-                        >
-                            Continue
-                        </AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
-
-            <LogoutButton></LogoutButton>
+        <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6">
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
+                <AlertTable />
+            </div>
         </div>
     )
 };
