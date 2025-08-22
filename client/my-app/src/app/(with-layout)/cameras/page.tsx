8f1fb0a2c842d82847929d9ed146ef97f36bce5e
MERCURY <wanasart2@gmail.com>
2025-08-22 10:35:57 +0700

    style: lock จำนวน column ของแต่ละขนาดหน้าจอ

--- client/my-app/src/app/(with-layout)/cameras/page.tsx
+++ client/my-app/src/app/(with-layout)/cameras/page.tsx
@@ -5,7 +5,7 @@ const base = process.env.NEXT_PUBLIC_APP_URL!;
 
 export default async function CamerasPage() {
 
-  const res = await fetch(`${base}/api/cameras`, { cache: "no-store" });
+  const res = await fetch(`${base}/api/cameras/`, { cache: "no-store" });
   if (!res.ok) {
     throw new Error("Failed to load cameras");
   }
@@ -17,7 +17,8 @@ export default async function CamerasPage() {
       <StatusCard.DashboardSummaryCameraSection></StatusCard.DashboardSummaryCameraSection>
 
       <div className="rounded-lg bg-[var(--color-white)] shadow-md p-6 ">
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
+        {/* <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6"> */}
+        <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
           {cameras.map((cam) => (
             <CameraCard key={cam.id} cam={cam} />
           ))}
@@ -26,32 +27,3 @@ export default async function CamerasPage() {
     </div>
   );
 }
-
-// "use client";
-// import { useEffect, useState } from "react";
-// import CameraCard, { Camera } from "../components/CameraCard";
-
-// export default function CamerasPage() {
-//   const [cameras, setCameras] = useState<Camera[]>([]);
-//   const [loading, setLoading] = useState(true);
-//   const [err, setErr] = useState("");
-
-//   useEffect(() => {
-//     fetch("/api/cameras")
-//       .then((res) => res.json())
-//       .then((data: Camera[]) => setCameras(data))
-//       .catch((e) => setErr(e.message))
-//       .finally(() => setLoading(false));
-//   }, []);
-
-//   if (loading) return loading;
-//   if (err) throw new Error("Failed to load cameras");
-
-//   return (
-//     <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6">
-//       {cameras.map((cam) => (
-//         <CameraCard key={cam.cam_id} cam={cam} />
-//       ))}
-//     </div>
-//   );
-// }
