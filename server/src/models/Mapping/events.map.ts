b192df0afe28ac6a97d94a6bdfe28a15b65e72e9
Jfyy-x <fasaiswy502@gmail.com>
2025-08-22 14:20:10 +0700

    feat(events): get by id

--- server/src/models/Mapping/events.map.ts
+++ server/src/models/Mapping/events.map.ts
@@ -1,1 +1,21 @@
 import { splitDateTime } from "./timeDate.map"
+import * as Model from "../../models/events.model"
+
+export const mapToEvent = (row: any): Model.Events => {
+
+    return {
+        id: row.evt_id,
+        icon: row.evt_icon,
+        name: row.evt_name,
+        description: row.evt_description,
+        is_use: row.evt_is_use
+    }
+};
+
+export const mapToEventDelete = (row: any): Model.EventSafeDelete => {
+
+    return {
+        id: row.evt_id,
+        is_use: row.evt_is_use
+    }
+};
